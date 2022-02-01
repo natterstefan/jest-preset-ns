a61221a4f0ef30fc45ace8b5465bca150e2372de
Stefan Natter <stefan@natter.at>
2022-02-01 20:42:48 +0100

    test: added tests for validating graphql support

--- packages/example-app/__tests__/index.test.js
+++ packages/example-app/__tests__/index.test.js
@@ -1,7 +1,25 @@
+import gql from 'graphql-tag'
+
 import foo from '..'
+
+// TODO: only here for testing if the jest preset supports graphql
+// eslint-disable-next-line no-unused-vars
+import testQuery from './test.graphql'
+
+// TODO: only here for testing if the jest preset supports graphql
+// eslint-disable-next-line no-unused-vars
+const query = gql`
+  {
+    users {
+      id
+      firstName
+      lastName
+    }
+  }
+`
 
 describe('foo', () => {
   it('returns bar', () => {
-    expect(foo()).toStrictEqual('bar')
+    expect(foo()).toBe('bar')
   })
 })
