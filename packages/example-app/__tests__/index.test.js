b2ee7f9bd9bc9e838f40734a2c737048d9092075
Stefan Natter <stefan@natter.at>
2022-02-01 20:43:52 +0100

    fixup! fixed comment

--- packages/example-app/__tests__/index.test.js
+++ packages/example-app/__tests__/index.test.js
@@ -2,11 +2,11 @@ import gql from 'graphql-tag'
 
 import foo from '..'
 
-// TODO: only here for testing if the jest preset supports graphql
+// NOTE: only here for testing if the jest preset supports graphql
 // eslint-disable-next-line no-unused-vars
 import testQuery from './test.graphql'
 
-// TODO: only here for testing if the jest preset supports graphql
+// NOTE: only here for testing if the jest preset supports graphql
 // eslint-disable-next-line no-unused-vars
 const query = gql`
   {
