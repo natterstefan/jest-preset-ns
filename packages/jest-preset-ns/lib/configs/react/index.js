a8869f7ce32454e3aeed02559bf351244fff8178
Stefan Natter <stefan@natter.at>
2022-08-13 14:33:29 +0200

    feat: mark enzyme as deprecated

--- packages/jest-preset-ns/lib/configs/react/index.js
+++ packages/jest-preset-ns/lib/configs/react/index.js
@@ -1,6 +1,9 @@
 const createPreset = require('../create-jest-preset')
 
 /**
+ * Inspired by
+ * @see https://github.com/americanexpress/amex-jest-preset-react/tree/61ae3106b8ae3483b8953b01fc32d70660af6688
+ *
  * @type {import('@jest/types').Config.InitialOptions}
  */
 const jestReactConfig = {
@@ -8,18 +11,6 @@ const jestReactConfig = {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  /**
-   * TODO: setupFilesAfterEnv was not working properly.
-   *
-   * It always threw the missing enzyme adapter error. One must add it
-   * manually right now.
-   *
-   * Find out what eg. amex-jest-preset-react does differently:
-   * @see https://github.com/americanexpress/amex-jest-preset-react/tree/v6.0.0
-   *
-   * This did not work as well:
-   * setupFilesAfterEnv: ['jest-preset-ns/presets/react/jest-setup.js'],
-   */
   setupFilesAfterEnv: [require.resolve('../../../presets/react/jest-setup.js')],
   // TODO: use testEnvironment: 'jest-environment-jsdom-global' instead?
   testEnvironment: 'jest-environment-jsdom',
--- packages/jest-preset-ns/presets/react/jest-setup.js
+++ packages/jest-preset-ns/presets/react/jest-setup.js
@@ -1,10 +1,24 @@
-const { configure } = require('enzyme')
-/**
- * unofficial React@17 adapter, until the offical one is released
- *
- * @see https://github.com/enzymejs/enzyme/issues/2429#issuecomment-831006991
- * @see https://github.com/enzymejs/enzyme/pull/2430
- */
-const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
+/* eslint-disable global-require */
+const semver = require('semver')
+const colors = require('colors')
+const reactVersion = require('react/package.json').version
 
-configure({ adapter: new Adapter() })
+if (semver.lt(reactVersion, '18.0.0')) {
+  // eslint-disable-next-line no-console
+  console.log(
+    colors.red(
+      'ATTENTION: Enzyme is deprecated and its support will be removed in the next major version! Visit https://github.com/natterstefan/jest-preset-ns/issues/61 for more details.',
+    ),
+  )
+
+  const { configure } = require('enzyme')
+  /**
+   * unofficial React@17 adapter, until the offical one is released
+   *
+   * @see https://github.com/enzymejs/enzyme/issues/2429#issuecomment-831006991
+   * @see https://github.com/enzymejs/enzyme/pull/2430
+   */
+  const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
+
+  configure({ adapter: new Adapter() })
+}
